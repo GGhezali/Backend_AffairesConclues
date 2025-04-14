1288a5559dfd83bdcf31fc23a12bd676bd954b81
Djoudi <djosamir@yahoo.fr>
2025-04-14 17:52:37 +0200

    reset backend

--- models/articles.js
+++ models/articles.js
@@ -1,28 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const localisationSchema = mongoose.Schema({
-    adresse: String,
-    longitude: Number,
-    latitude: Number,
-})
-
-const articleSchema = mongoose.Schema({
- titre: String,
- categorie: { type: mongoose.Schema.Types.ObjectId, ref: 'categories' },
- etat: { type: mongoose.Schema.Types.ObjectId, ref: 'etats' },
- description: String,
- auteur: { type: mongoose.Schema.Types.ObjectId, ref: 'auteurs' },
- editeur: { type: mongoose.Schema.Types.ObjectId, ref: 'editeurs' },
- startPrice: Number,
- currentPrice: Number,
- localisation: localisationSchema,
- photoUrl: String,
- annonceur: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
- acheteur: { type: mongoose.Schema.Types.ObjectId, ref: 'users' }, 
- timer: Date,
- isSold: Boolean,
+  adresse: String,
+  longitude: Number,
+  latitude: Number,
 });
 
-const Article = mongoose.model('articles', articleSchema);
+const articleSchema = mongoose.Schema({
+  titre: String,
+  categorie: { type: mongoose.Schema.Types.ObjectId, ref: "categories" },
+  etat: { type: mongoose.Schema.Types.ObjectId, ref: "etats" },
+  description: String,
+  auteur: { type: mongoose.Schema.Types.ObjectId, ref: "auteurs" },
+  editeur: { type: mongoose.Schema.Types.ObjectId, ref: "editeurs" },
+  startPrice: Number,
+  currentPrice: Number,
+  localisation: localisationSchema,
+  photoUrl: String,
+  annonceur: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  acheteur: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  timer: Date,
+  isUsed: Boolean,
+});
+
+const Article = mongoose.model("articles", articleSchema);
 
 module.exports = Article;
